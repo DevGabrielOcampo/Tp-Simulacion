18fa9c10fc6cf6c18a780664a03ee0aaee1cce87
Matias Marchetti <mhmarchetti96@gmail.com>
2025-03-29 18:38:43 -0300

    Se crea el formulario

---
+++ Frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import Datos from './components/Datos/Datos';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Datos /> 
+    </div>
+  );
+}
+
+export default App;

072cf324487e176d009e52b88f476d59ee3aa153
Gabriel Alejandro Ocampo <93368@sistemas.frc.utn.edu.ar>
2025-03-29 19:10:23 -0300

    Backend Terminado V1.0

--- Backend/src/main/java/grupo15/main/Controller/Controller.java
+++ Backend/src/main/java/grupo15/main/Controller/Controller.java
@@ -29,11 +29,11 @@ public class Controller {
             case 1: // Distribución uniforme (Ejemplo para probar http://localhost:8080/api/muestra/1000,1,10,20,0,0,0)
                 resultado = Generadores.generadorUniforme(muestra.intValue(), a, b);
                 break;
-            case 2: // Distribución exponencial ()
+            case 2: // Distribución exponencial (Ejemplo para probar http://localhost:8080/api/muestra/1000,2,0,0,0,0,1)
                 resultado = Generadores.generadorExponencial(muestra, lambda);
                 break;
             case 3: // Distribución normal
-                resultado = Generadores.generadorNormal(muestra, desviacion, desviacion);
+                resultado = Generadores.generadorNormal(muestra, media, desviacion);
                 break;
             default: // Opción no válida
                 throw new IllegalArgumentException("Opción no válida. Selecciona 1, 2 o 3.");
